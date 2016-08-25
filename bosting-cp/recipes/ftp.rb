180f7e59be065d65d0c8dea867ab3842231c9abe
Alexander Zubkov <info@zubkov.info>
2016-08-25 13:59:24 +0300

    Add ftp cookbook

---
+++ bosting-cp/recipes/ftp.rb
@@ -0,0 +1,56 @@
+if node['platform'] == 'freebsd'
+  package 'libsodium'
+  freebsd_port 'pure-ftpd' do
+    options docs: false, examples: false, largefile: true, mysql: true, pam: false, peruserlimits: true,
+            scrypt: false, throttling: true, tls: false, virtualchroot: false
+  end
+  dest_path = '/usr/local/etc/pure-ftpd.conf'
+end
+
+if node['platform'] == 'debian'
+  package 'pure-ftpd-mysql'
+end
+
+service 'pure-ftpd' do
+  supports status: true, restart: true, reload: false
+  action :nothing
+end
+
+pureftpd_password = generate_random_password(16)
+
+mysql_database_user 'pureftpd' do
+  connection node['bosting-cp']['mysql_connection_info']
+  database_name 'bosting-cp'
+  table 'pureftpd'
+  privileges [:select]
+  password pureftpd_password
+  action :grant
+  not_if { ::File.exist?(dest_path) }
+end
+
+if node['platform'] == 'freebsd'
+  template '/usr/local/etc/pureftpd-mysql.conf' do
+    source 'pureftpd-mysql.conf.erb'
+    variables(
+        password: pureftpd_password
+    )
+    notifies :reload, 'service[pure-ftpd]'
+    not_if { ::File.exist?(dest_path) }
+  end
+
+  template dest_path do
+    source 'pure-ftpd.conf.erb'
+    variables(
+        ip: node['bosting-cp']['ip']
+    )
+    notifies :reload, 'service[pure-ftpd]'
+  end
+end
+
+if node['platform'] == 'debian'
+  # TODO
+end
+
+service 'pure-ftpd' do
+  action [:enable, :start]
+end
