7bb2ff91b7ee967b4f6abc42d1378224a59d1bc9
Alexander Zubkov <info@zubkov.info>
2016-08-30 14:04:24 +0300

    Fix uid and user shell

--- bosting-generator/resources/system_user.rb
+++ bosting-generator/resources/system_user.rb
@@ -1,15 +1,15 @@
 resource_name :system_user
 
 property :name, String, name_property: true
-property :group, String, required: true
 property :uid, Fixnum, required: true
+property :group, String, required: true
 property :shell, String, required: true
 
 action :create do
   user name do
-    uid uid
+    uid new_resource.uid
     group new_resource.group
-    shell shell
+    shell new_resource.shell
     home "/home/#{name}"
   end
 
