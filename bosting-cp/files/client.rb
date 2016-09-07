b3bf6bca2082800b0b0e7732f82a97a033f58bb8
Alexander Zubkov <info@zubkov.info>
2016-09-07 17:45:32 +0300

    Make `chef-client` run `bosting-generator` recipe

---
+++ bosting-cp/files/client.rb
@@ -0,0 +1,4 @@
+node_name       'bosting-generator'
+log_location    '/var/log/chef-client'
+local_mode      true
+cookbook_path   ['/root/cookbooks']
--- bosting-cp/recipes/chef_client.rb
+++ bosting-cp/recipes/chef_client.rb
@@ -1,3 +1,12 @@
+directory '/etc/chef' do
+  mode 0700
+end
+
+cookbook_file '/etc/chef/client.rb' do
+  source 'client.rb'
+  mode 0600
+end
+
 cookbook_file '/usr/local/etc/rc.d/chef-client' do
   source 'chef-client'
   mode 0755
