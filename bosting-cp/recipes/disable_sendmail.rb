8bf9349c7df1b535c49982565a0f07e9de1f3d3c
Alexander Zubkov <info@zubkov.info>
2016-10-06 18:04:04 +0300

    Setup periodic.conf for sendmail

--- bosting-cp/recipes/disable_sendmail.rb
+++ bosting-cp/recipes/disable_sendmail.rb
@@ -6,3 +6,17 @@ end
 service 'sendmail' do
   action :stop
 end
+
+file '/etc/periodic.conf'
+
+%w(
+  daily_clean_hoststat_enable="NO"
+  daily_status_mail_rejects_enable="NO"
+  daily_status_include_submit_mailq="NO"
+  daily_submit_queuerun="NO"
+).each do |add_line|
+  append_if_no_line "Add '#{add_line}' to /etc/periodic.conf" do
+    path '/etc/periodic.conf'
+    line add_line
+  end
+end
